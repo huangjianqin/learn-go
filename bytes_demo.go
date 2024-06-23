a38481fa53785a8cef005581ad2c116708b24f25
huangjianqin <459564034@qq.com>
2024-06-23 11:26:42 +0800

    bytes array or slice cap()和len()测试

--- bytes_demo.go
+++ bytes_demo.go
@@ -1,9 +1,17 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 func main() {
 	bs := []byte("1234asdff12321")
 	println(bytes.Contains(bs, []byte("123")))
 	println(bytes.Count(bs, []byte("123")))
+
+	bs1 := make([]byte, 16)
+	fmt.Printf("len(bs1):%v , cap(bs1):%v\r\n", len(bs1), cap(bs1))
+	bss1 := bs1[:8]
+	fmt.Printf("len(bss1):%v , cap(bss1):%v\n", len(bss1), cap(bss1))
 }
